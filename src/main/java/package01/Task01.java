37f1f0d58bf9c8a882dcc3d9d2986f03ab6764ec
ilgar222 <ilqar6117@gmail.com>
2023-03-14 01:40:37 +0400

    code number one

--- src/main/java/package01/Task01.java
+++ src/main/java/package01/Task01.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Task01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
 
-        if (num > 0) {
-            System.out.println("Positive");
-        } else if (num < 0) {
-            System.out.println("Negative");
+        if (a % b != 0) {
+            System.out.print(a / b);
+            System.out.print(" ");
+            System.out.println(a % b);
 
         } else {
-            System.out.println("Zero");
+            System.out.println("Divisible");
         }
+
     }
 }
