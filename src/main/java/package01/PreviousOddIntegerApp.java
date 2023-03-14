21aa3b7e17d727b0d90edaf958704570a6ac854d
ilgar222 <ilqar6117@gmail.com>
2023-03-14 15:48:53 +0400

    code number one

---
+++ src/main/java/package01/PreviousOddIntegerApp.java
@@ -0,0 +1,17 @@
+package package01;
+
+import java.util.Scanner;
+
+public class PreviousOddIntegerApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num - 1);
+        } else {
+            System.out.println(num - 2);
+
+        }
+    }
+}
