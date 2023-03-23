fe481e141042fa96038fc482b74cb33d72d2a21e
ilgar222 <ilqar6117@gmail.com>
2023-03-24 00:38:07 +0400

    e-olymp examples added

---
+++ src/main/java/homework/RectangularApp.java
@@ -0,0 +1,16 @@
+package homework;
+
+import java.util.Scanner;
+
+public class RectangularApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
