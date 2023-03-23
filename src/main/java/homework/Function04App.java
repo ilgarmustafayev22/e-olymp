987b6cd8fe30287ca89cae8418cc3f14a34f7797
ilgar222 <ilqar6117@gmail.com>
2023-03-23 17:35:20 +0400

    e-olymp examples added

---
+++ src/main/java/homework/Function04App.java
@@ -0,0 +1,11 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Function04App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        System.out.println(1 + x + (x * x));
+    }
+}
