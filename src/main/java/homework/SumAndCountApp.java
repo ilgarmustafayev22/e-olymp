1e5ac536d5956f7cd1fe7a7fe94fac5a5aeb1932
ilgar222 <ilqar6117@gmail.com>
2023-03-26 22:53:16 +0400

    e-olymp examples added

---
+++ src/main/java/homework/SumAndCountApp.java
@@ -0,0 +1,18 @@
+package homework;
+
+import java.util.Scanner;
+
+public class SumAndCountApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        int sum=0;
+        while (sc.hasNext()){
+
+            int a= sc.nextInt();
+            count++;
+            sum+=a;
+        }
+        System.out.print(count +" "+sum);
+    }
+}
