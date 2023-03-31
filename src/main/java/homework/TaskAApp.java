b0f7344b296c70c35a1a6a46f0afaec245fea656
ilgar222 <ilqar6117@gmail.com>
2023-03-31 22:19:21 +0400

    e-olymp examples added

---
+++ src/main/java/homework/TaskAApp.java
@@ -0,0 +1,17 @@
+package homework;
+
+import java.util.Scanner;
+
+public class TaskAApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double maxSalary=-1;
+        double salary;
+        while (sc.hasNext()){
+            salary=sc.nextDouble();
+            if(maxSalary<salary) {
+                maxSalary=salary;
+            }
+        }System.out.printf("%.2f",maxSalary);
+    }
+}
