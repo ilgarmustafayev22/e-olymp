4699a8b6a58f368639c464523acfb735ee4f8f77
ilgar222 <ilqar6117@gmail.com>
2023-04-02 14:40:39 +0400

    e-olymp examples added

--- src/main/java/homework/FunctionApp.java
+++ src/main/java/homework/FunctionApp.java
@@ -4,19 +4,14 @@ import java.util.Scanner;
 
 public class FunctionApp {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double x = scan.nextDouble();
-        double y = scan.nextDouble();
-        if (x > 0 && y > 0) {
-            System.out.println(1);
-        } else if (x < 0 && y > 0) {
-            System.out.println(2);
-        } else if (x < 0 && y < 0) {
-            System.out.println(3);
-        } else if (x > 0 && y < 0) {
-            System.out.println(4);
-        }else {
-            System.out.println(0);
-        }
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        double z = sc.nextDouble();
+        double maxXY = Math.max(x, y);
+        double maxYZ = Math.max(y, z);
+        double sum = x + y + z;
+        double min = Math.min(maxXY, maxYZ);
+        System.out.printf("%.2f", Math.min(sum, min));
     }
 }
